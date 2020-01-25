91f36dc0d5bdc0387e21044f4d67818dd75e95d3
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-01-25 22:09:36 +0700

    update

--- src/main/java/com/demo/seoacount/controller/SeoAcountController.java
+++ src/main/java/com/demo/seoacount/controller/SeoAcountController.java
@@ -3,6 +3,7 @@ package com.demo.seoacount.controller;
 import com.demo.seoacount.model.Game;
 import com.demo.seoacount.model.ListMenu;
 import com.demo.seoacount.model.MenuItem;
+import com.demo.seoacount.utils.Contant;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -64,6 +65,28 @@ public class SeoAcountController {
         allLstGame.add(new Game(11, "Battle Royale", 75, "1.100.000", "7.000", "10", 2 ));
         allLstGame.add(new Game(11, "Fortnite Battle Royale", 10, "1.500.000", "70.000", "11", 3 ));
         allLstGame.add(new Game(12, "Destity", 30, "1.710.000", "760.000", "12", 3 ));
+        allLstGame.add(new Game(13, "Con cha", 12, "1.700.000", "700.000", "1" , 5));
+        allLstGame.add(new Game(14, "Liên Quân", 73, "1.700.000", "700.000", "2", 3 ));
+        allLstGame.add(new Game(15, "identity V", 70, "1.700.000", "700.000", "3", 5 ));
+        allLstGame.add(new Game(16,"mario", 72, "1.700", "20.000", "4", 4 ));
+        allLstGame.add(new Game(17, "Fortnite Battle", 50, "1.000.000", "500.000", "9", 5 ));
+        allLstGame.add(new Game(18,"Pikachu", 50, "1.000.000", "500.000", "9", 4 ));
+        allLstGame.add(new Game(19,"Fortnite Battle", 70, "1.700.000", "501.000", "9", 3 ));
+        allLstGame.add(new Game(20, "Battle Royale", 75, "1.100.000", "7.000", "10", 2 ));
+        allLstGame.add(new Game(21, "Fortnite Battle Royale", 10, "1.500.000", "70.000", "11", 3 ));
+        allLstGame.add(new Game(22, "Destity", 30, "1.710.000", "760.000", "12", 3 ));
+        allLstGame.add(new Game(23, "Con cha", 12, "1.700.000", "700.000", "11" , 5));
+        allLstGame.add(new Game(24, "Liên Quân", 73, "1.700.000", "700.000", "2", 3 ));
+        allLstGame.add(new Game(25, "identity V", 70, "1.700.000", "700.000", "3", 5 ));
+        allLstGame.add(new Game(26,"mario", 72, "1.700", "20.000", "3", 4 ));
+        allLstGame.add(new Game(27, "LOL", 70, "1.400.000", "800.000", "5", 2 ));
+        allLstGame.add(new Game(28,"Fortnite Royale", 80, "1.700.000", "700.000", "7" ,3));
+        allLstGame.add(new Game(29, "Fortnite Battle", 50, "1.000.000", "500.000", "8", 5 ));
+        allLstGame.add(new Game(30,"Pikachu", 50, "1.000.000", "500.000", "9", 4 ));
+        allLstGame.add(new Game(31,"Fortnite Battle", 70, "1.700.000", "501.000", "11", 3 ));
+        allLstGame.add(new Game(32, "Battle Royale", 75, "1.100.000", "7.000", "10", 2 ));
+        allLstGame.add(new Game(33, "Fortnite Battle Royale", 10, "1.500.000", "70.000", "11", 3 ));
+        allLstGame.add(new Game(34, "Destity", 30, "1.710.000", "760.000", "10", 3 ));
 
 
 //        insert data for good game
@@ -81,15 +104,34 @@ public class SeoAcountController {
     }
 
     @RequestMapping("/home")
-    public String home(Model model) {
+    public String home(Model model,  @RequestParam(value = "Page", defaultValue = "0") int page,
+                       @RequestParam(value = "filter", defaultValue = "0") int filter) {
         int basketSize = (basketGame.size());
         int loveSize = (loveGame.size());
+        List<Game> gameOfPage = new ArrayList<>();
+
+        int index = page * Contant.PAGE_SIZE;
+        int lengthGame = page * Contant.PAGE_SIZE + Contant.PAGE_SIZE > allLstGame.size() ? allLstGame.size() :
+                page * Contant.PAGE_SIZE + Contant.PAGE_SIZE;
+
+        for(int i = index; i< lengthGame; i++) {
+            Game game = allLstGame.get(i);
+            gameOfPage.add(game);
+        }
+
+        int totalPage = allLstGame.size() % Contant.PAGE_SIZE != 0
+                ? (allLstGame.size()/Contant.PAGE_SIZE )
+                : (allLstGame.size()/Contant.PAGE_SIZE -1);
+        model.addAttribute("listGame", gameOfPage);
+//        sau sẽ ko dùng size nưa sẽ dùng hàm đếm số phần tử trong database
+        model.addAttribute("totalPage", totalPage);
+        model.addAttribute("currentPage", page);
 
         model.addAttribute("lstMenu", lstMenu );
         model.addAttribute("lstItem", lstItem );
         model.addAttribute("basketGame", basketGame);
         model.addAttribute("loveGame", loveGame);
-        model.addAttribute("allLstGame", allLstGame);
+        model.addAttribute("gameOfPage", gameOfPage);
         model.addAttribute("goodLstGame", goodLstGame);
         model.addAttribute("basketSize", basketSize);
         model.addAttribute("loveSize", loveSize);
@@ -164,6 +206,7 @@ public class SeoAcountController {
                 basketGame.add(g);
             }
         }
+
         model.addAttribute("lstMenu", lstMenu );
         model.addAttribute("lstItem", lstItem );
         model.addAttribute("basketGame", basketGame);
---
+++ src/main/java/com/demo/seoacount/utils/Contant.java
@@ -0,0 +1,5 @@
+package com.demo.seoacount.utils;
+
+public class Contant {
+    public final static int PAGE_SIZE = 12;
+}
